d26c90808c1881457a6b49859037b715874b0355
kozinvl <vladiksystem@gmail.com>
2018-05-19 22:41:25 +0300

    refactoring

--- comments_sprintone/front/comm_page.py
+++ comments_sprintone/front/comm_page.py
@@ -19,25 +19,25 @@ class CommentsPage(object):
 
     def click_duplicate_comment(self):
         duplicate_comment = self.driver.find_element_by_xpath(
-            Duplicate().DUPLICATE_BUTTON)
+            Duplicate.DUPLICATE_BUTTON)
         duplicate_comment.click()
 
     def click_edit_comment(self):
         """click on edit button and move to edit page"""
         edit_comment = self.driver.find_element_by_xpath(
-            Edit().EDIT_BUTTON)
+            Edit.EDIT_BUTTON)
         edit_comment.click()
 
     def delete_comment(self):
         """click on delete button and move to edit page"""
         delete_comment = self.driver.find_element_by_xpath(
-            Delete().DELETE_BUTTON)
+            Delete.DELETE_BUTTON)
         delete_comment.click()
 
     def confirm_action(self):
         """confirmation positive action with button (yes)"""
         confirm_btn = self.driver.find_element_by_xpath(
-            Delete().CONFIRMATION_BUTTON)
+            Delete.CONFIRMATION_BUTTON)
         confirm_btn.click()
 
     def filling_text_comment(self, add_text: str, is_filled: False):
@@ -50,7 +50,7 @@ class CommentsPage(object):
     def filling_number(self, add_number: str, is_filled: False):
         """filling comment number field by number"""
         number_field = self.driver.find_element_by_id(
-            NewComm().SEND_NUMBER)
+            NewComm.SEND_NUMBER)
         number_field.click()
         if is_filled:
             number_field.clear()
@@ -59,38 +59,38 @@ class CommentsPage(object):
     def save(self):
         """save all data on new comment page"""
         save_btn = self.driver.find_element_by_xpath(
-            Other().SAVE_BUTTON)
+            Other.SAVE_BUTTON)
         save_btn.click()
 
     def save_return(self):
         """save and return to main page"""
         save_return_btn = self.driver.find_element_by_xpath(
-            Other().SAVE_RETURN_BUTTON)
+            Other.SAVE_RETURN_BUTTON)
         save_return_btn.click()
 
     def chose_one_category_comment(self):
         """chose category on new comment page"""
         one_category = self.driver.find_element_by_xpath(
-            NewComm().choose_random_category_cat())
+            NewComm.choose_random_category_cat())
         one_category.click()
         one_category.click()
         confirm_category = self.driver.find_element_by_name(
-            NewComm().ONE_CATEGORY)
+            NewComm.ONE_CATEGORY)
         confirm_category.click()
 
     def choose_all_category_comment(self):
         """chose all category on new comment page"""
         self.driver.find_element_by_name(
-            NewComm().ALL_CATEGORIES).click()
+            NewComm.ALL_CATEGORIES).click()
 
     def get_error_length(self):
         actual_error = self.driver.find_element_by_xpath(
-            Other().ERROR_LENGTH).text
+            Other.ERROR_LENGTH).text
         return actual_error
 
     def get_error_symbol(self):
         actual_error = self.driver.find_element_by_id(
-            Other().ERROR_SYMBOL).text
+            Other.ERROR_SYMBOL).text
         return actual_error
 
     def get_check_popup(self):
@@ -107,14 +107,14 @@ class CommentsPage(object):
     def choose_all_categories_main(self):
         """chose all categories on main page"""
         all_category = self.driver.find_elements_by_name(
-            Other().ALL_CATEGORIES_MAIN)
+            Other.ALL_CATEGORIES_MAIN)
         for each_category in all_category:
             each_category.click()
 
     def choose_one_category_main(self):
         """chose one category on main page"""
         one_category = self.driver.find_element_by_xpath(
-            Other().choose_one_random_cat())
+            Other.choose_one_random_cat())
         one_category.click()
 
     def get_name_title_page(self):
